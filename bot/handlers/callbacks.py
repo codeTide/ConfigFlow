8d46c5837c9b2fb9e834fcd8c60505c5ff91dbed
EmadHabibnia <120003549+Emadhabibnia1385@users.noreply.github.com>
2026-04-06 10:38:33 +0330

    Update callbacks.py

--- bot/handlers/callbacks.py
+++ bot/handlers/callbacks.py
@@ -51,6 +51,7 @@ from ..admin.renderers import (
     _show_admin_user_detail_msg, _show_admin_assign_config_type, _fake_call,
     _show_admin_panels, _show_panel_packages, _show_panel_edit,
 )
+from ..admin.backup import _send_backup
 
 @bot.callback_query_handler(func=lambda c: True)
 def on_callback(call):
